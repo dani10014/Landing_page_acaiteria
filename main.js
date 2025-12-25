d06e6f300d8c0eecb82c2a88334a7646fd97ea67
Daniel <dijalmaduartefleitas0@gmail.com>
2025-12-25 16:55:15 -0400

    Adicionando aviso de adição do carrinho

---
+++ main.js
@@ -0,0 +1,13 @@
+let botoesAdicionarCarrinho = document.querySelectorAll(".adicionar-carrinho");
+let blocoAlerta = document.querySelector(".alerta");
+
+
+
+botoesAdicionarCarrinho.forEach(button => {
+    button.addEventListener("click", () => {
+        blocoAlerta.classList.add("alerta-ativo")
+        setTimeout(() => {
+            blocoAlerta.classList.remove("alerta-ativo")
+        }, 700);
+    });
+});
