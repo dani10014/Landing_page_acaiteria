28e35175bf8bb47c2e91a0ec27d5dd54ccf28c4e
Daniel <dijalmaduartefleitas0@gmail.com>
2025-12-27 13:48:34 -0400

    Adicionando carrosel de imagens do açai

--- main.js
+++ main.js
@@ -2,12 +2,19 @@ let botoesAdicionarCarrinho = document.querySelectorAll(".adicionar-carrinho");
 let blocoAlerta = document.querySelector(".alerta");
 
 
-
 botoesAdicionarCarrinho.forEach(button => {
     button.addEventListener("click", () => {
         blocoAlerta.classList.add("alerta-ativo")
         setTimeout(() => {
             blocoAlerta.classList.remove("alerta-ativo")
         }, 1000);
+    });
+});
+
+
+$(document).ready(function() {
+    $(".carrosel").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
     });
 });
